964a750e8a0b247ef46e22ad40ecb71d7d28b116
Hunter-L <389457415@qq.com>
2015-11-06 17:02:02 +0800

    Frist commit.

---
+++ src/com/example/fragmentbewtpractice/News.java
@@ -0,0 +1,31 @@
+/**2015年11月5日
+ * 
+ */
+package com.example.fragmentbewtpractice;
+
+/**
+ * @author Administrator
+ *
+ */
+public class News {
+    
+    private String title;
+    
+    private String content;
+    
+    public String getTitle() {
+	return title;
+    }
+    
+    public void setTitle(String title) {
+	this.title =title;
+    }
+    
+    public String getContent() {
+	return content;
+    }
+    
+    public void setContent(String content) {
+	this.content = content;
+    }
+}
---
+++ src/com/example/fragmentbewtpractice/NewsContentFragment.java
@@ -0,0 +1,38 @@
+/**2015年11月5日
+ * 
+ */
+package com.example.fragmentbewtpractice;
+
+import android.app.Fragment;
+import android.R.raw;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author Administrator
+ *
+ */
+public class NewsContentFragment extends Fragment {
+    
+    private View view;
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	    Bundle savedInstanceState) {
+	view = inflater.inflate(R.layout.news_content_frag, container, false);
+	return view;
+    }
+    
+    public void refresh(String newsTitle, String newsContent) {
+	View visibilityLayout = view.findViewById(R.id.visibility_layout);
+	visibilityLayout.setVisibility(View.VISIBLE);
+	TextView newsTitleText = (TextView) view.findViewById(R.id.news_title);
+	TextView newsContentText = (TextView) view.findViewById(R.id.news_content);
+	newsTitleText.setText(newsTitle); //刷新新闻的标题
+	newsContentText.setText(newsContent);  //刷新新闻的内容
+    }
+
+}
